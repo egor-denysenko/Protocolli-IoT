76c458b0e6ab7c78ce55202f4fb51bd97d6b3944
Egor Denysenko <egor.denys0@gmail.com>
2022-01-13 14:34:10 +0100

    Basic Server AMQP recieve from rabbitMQ queue

--- server/ApiGateway/plugins/psql.js
+++ server/ApiGateway/plugins/psql.js
@@ -3,6 +3,6 @@
 const fp = require('fastify-plugin')
 const db = require('pg-promise')()
 module.exports = fp(async function(fastify, opts) {
-    const psql = db(process.env.POSTGRESQL_URI)
-    fastify.decorate('psql', psql);
+    // const psql = db(process.env.POSTGRESQL_URI)
+    // fastify.decorate('psql', psql);
 })
